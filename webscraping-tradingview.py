4766a4fa9067a5cf1dd30476be50c7b0de1fff3f
owendecicco1 <owen_decicco1@baylor.edu>
2022-11-01 12:28:28 -0500

    Next

--- webscraping-tradingview.py
+++ webscraping-tradingview.py
@@ -12,13 +12,19 @@ from bs4 import BeautifulSoup
 ##  > sudo "./Install Certificates.command"
 
 
-url = 'https://www.tradingview.com/markets/stocks-usa/market-movers-gainers/'
+#url = 'https://www.tradingview.com/markets/stocks-usa/market-movers-gainers/'
+url =  "https://www.webull.com/quote/us/gainers"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
-		
+req = Request(url, headers = headers)
 
+webpage = urlopen(req).read()
 
+soup = BeautifulSoup(webpage, "html.parser")
 
+title  = soup.title
+
+print(title.text)
 
 
 
@@ -32,4 +38,36 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML
 #Text: nameList = Objfind(text="the prince")
 #Limit = find with limit of 1
 #keyword: allText = Obj.find(id="title",class="text")
+
+tablecells = soup.findAll("div", attrs = {"class": "table-cell"})
+
+print(tablecells[0].text)
+#will print out a list
+#Number 1 is the actual text
+
+print(tablecells[0].text)
+print(tablecells[1].text)
+print(tablecells[3].text)
+print(tablecells[5].text)
+print(tablecells[6].text)
+
+print("Percentage Change: ",tablecells[3].text)
+print("High: ",tablecells[5].text)
+print("Low: ",tablecells[6].text)
+
+name = 1
+high = 5
+low = 6
+
+count = 1
+
+while count <= 5:
+    calc = ((float(tablecells[high].text) - float(tablecells[low].text)) / float(tablecells[low].text)) * 100
+    print(f'* Name: {tablecells[name].text} || High: {tablecells[high].text} || Low: {tablecells[low].text}')
+    print(calc)
+    name += 11
+    high += 11
+    low += 11
+    count += 11
+
 
